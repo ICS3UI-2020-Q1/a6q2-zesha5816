9e060964b12ef0ed9cc0415ff6ee3a35ad0c1840
zesha5816 <zesha5816@wrdsb.ca>
2020-10-08 15:22:04 +0000

    finished example

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that will populate a 10 element array (put   * values into an array) with integers
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a scanner 
+		Scanner input = new Scanner(System.in);
+		//output the message to user
+		System.out.println("Please enter in 10 integers to put into the array");
+		//make an array
+		int [] integers = new int[10];
+		//loops the code 10 times
+		for(int i = 0; i < 10; i++){
+			integers[i] = input.nextInt();
+		}
+		//output the message to user
+		System.out.println("Please enter a number to find");
+		//get input value
+		int number = input.nextInt();
+		//loops the code for how many times the value is in the code
+    for(int j = 0; j < integers.length; j++){
+			//if the integer is the same as the given number
+			if (integers[j] == number){
+				//print the location of the index number
+				System.out.println(number + " is located at index " + j);
+				//else skip the if statement
+			} else {}
+		}
   }
 }
